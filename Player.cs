79350935cf21a519d09a563694e06dd081d5e923
Serdar Özdemir <soezdemir3423@gmail.com>
2023-11-05 12:37:35 +0100

    Changed Entity Class's Position property to shorthand (int,int) and added getter. GetLook() implemented to draw the entity's position.

--- Player.cs
+++ Player.cs
@@ -8,22 +8,35 @@ namespace Fountain_of_Objects
 {
     public abstract class Entity
     {
-        protected Tuple<int, int> Position { get; set; }
+        protected (int, int) Position { get; set; }
         protected string Sign { get; set; }
-        protected bool isAlive { get; set; }
+        protected bool isAlive { get; set; } = true;
 
-        
+        public string GetSign() => Sign;
+
+        public bool GetIsAlive() => isAlive;
+
+        public (int, int) GetPosition() => Position;
+
+        public void GetLook()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write(Sign);
+            Console.ResetColor();
+        }
+
+        public void SetPosition((int, int) position)
+        {
+            this.Position = position;
+        }
     }
 
     public class Player : Entity
     {
         public Player()
         {
-            Position = new Tuple<int, int>(0, 0);
+            Position = (0, 0);
             Sign = "P";
-            isAlive = true;
         }
     }
-
-
 }
